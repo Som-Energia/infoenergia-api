ee4d71b2cd542abeff48b022fe6cb7e8ddc9a4a7
Joana Figueira <joana.figueira@somenergia.coop>
2019-10-14 09:39:08 +0200

    Update login request

--- mock-server/server.py
+++ mock-server/server.py
@@ -1,24 +1,37 @@
 import os
+import time
 
 import connexion
 from connexion.decorators.security import validate_scope
 from connexion.exceptions import OAuthScopeProblem
 
+from jose import JWTError, jwt
 
 BASE_DIR = os.path.dirname(os.path.abspath(__name__))
 
-
+JWT_ISSUER = 'com.zalando.connexion'
+JWT_SECRET = 'change_this'
+JWT_LIFETIME_SECONDS = 600
+JWT_ALGORITHM = 'HS256'
 
 def basic_auth(username, password):
     if username == 'admin' and password == 'secret':
-        info = {'sub': 'admin', 'scope': 'secret'}
-    elif username == 'foo' and password == 'bar':
-        info = {'sub': 'user1', 'scope': ''}
+        timestamp = _current_timestamp()
+        payload = {
+            "iss": JWT_ISSUER,
+            "iat": int(timestamp),
+            "exp": int(timestamp + JWT_LIFETIME_SECONDS),
+            "sub": str(username),
+        }
+        return jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
     else:
         # optional: raise exception for custom error response
         return None
 
-    return info
+
+def _current_timestamp() -> int:
+    return int(time.time())
+
 
 
 if __name__ == '__main__':
