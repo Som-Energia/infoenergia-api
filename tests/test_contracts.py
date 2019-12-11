679855394f6f58cbdc23d9b0d7c316f96335b9a6
Joana Figueira <joana.figueira@somenergia.coop>
2019-12-11 13:58:51 +0100

    Add: contracts and contractsId endpoints

--- tests/test_contracts.py
+++ tests/test_contracts.py
@@ -1,4 +1,8 @@
+import os
+import unittest
 from unittest import TestCase
+
+import yaml
 from api import app
 
 
@@ -6,10 +10,70 @@ class TestContracts(TestCase):
 
     def setUp(self):
         self.client = app.test_client
-        self.maxDiff = 0
+        self.maxDiff = None
 
-    def test__get_contracts(self):
-        request, response = self.client.get('/contracts')
+    def get_auth_token(self, username, password):
+        auth_body = {
+            'username': username,
+            'password': password
+        }
+
+        request, response = self.client.post('/auth', json=auth_body)
+        token = response.json.get('access_token', None)
+        return token
+
+    @db_session
+    def test__get_contracts_by_id(self):
+        user = User(
+            username='Joaquina',
+            password=pbkdf2_sha256.hash("12341234"),
+            id_partner=1,
+            is_superuser=True
+        )
+        token = self.get_auth_token('Joaquina', '12341234')
+
+        request, response = self.client.get(
+            '/contracts/'+json4test['contractId'],
+            headers={
+                'Authorization': 'Bearer {}'.format(token)
+            }
+        )
 
         self.assertEqual(response.status, 200)
-        self.assertDictEqual(response.json, {'hola': 'caracola'})
+        self.assertDictEqual(
+            response.json,
+            json4test['jsonContractById']
+        )
+        user.delete()
+
+    @db_session
+    def test__get_contracts___auth_user(self):
+        user = User(
+            username='Joaquina',
+            password=pbkdf2_sha256.hash("12341234"),
+            id_partner=1,
+            is_superuser=True
+        )
+        token = self.get_auth_token('Joaquina', '12341234')
+
+        params = {
+            'from_': '2019-10-03',
+            'to_': '2019-10-09',
+            'tariff': '2.0DHS',
+            'juridic_type': 'physical_person',
+        }
+        request, response = self.client.get(
+            '/contracts',
+            params=params,
+            headers={
+                'Authorization': 'Bearer {}'.format(token)
+            }
+        )
+
+        self.assertEqual(response.status, 200)
+        self.assertListEqual(
+            response.json,
+            json4test['jsonList']
+
+        )
+        user.delete()
