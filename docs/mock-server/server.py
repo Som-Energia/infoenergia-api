039670120c5756c2863ba3ee16cc7ba7a35dca19
Joana Figueira <joana.figueira@somenergia.coop>
2019-12-11 13:52:23 +0100

    Fix: update documentation tests

--- docs/mock-server/server.py
+++ docs/mock-server/server.py
@@ -3,11 +3,16 @@ import time
 
 import connexion
 import six
+import yaml
 from connexion.mock import MockResolver
 from jose import JWTError, jwt
 from werkzeug.exceptions import Unauthorized
 
 BASE_DIR = os.path.dirname(os.path.abspath(__name__))
+
+with open(os.path.join(BASE_DIR, 'tests/json4test.yaml')) as f:
+    json4test = yaml.load(f.read())
+
 
 JWT_ISSUER = 'som-energia'
 JWT_SECRET = 'j4h5gf6d78RFJTHGYH(/&%$·sdgfh'
@@ -30,7 +35,6 @@ def basic_auth(username, password):
         tokens[username] = token
         return token
     else:
-        # optional: raise exception for custom error response
         return ('Invalid credentials', 401)
 
 
@@ -47,330 +51,11 @@ def decode_token(token):
 
 
 def get_contract_by_id(user, token_info, contractId):
-    return {'contractId': contractId,
-            'ownerId': 'ownerId-123',
-            'payerId': 'payerId-123',
-            'power': 23,
-            'power_':
-            {
-                'power': 23,
-                'dateStart': '2019-10-20',
-                'dateEnd': '2019-10-21'
-            },
-            'powerHistory':
-            [{
-                'power': 23,
-                'dateStart': '2019-10-20',
-                'dateEnd': '2019-10-21'
-            },
-                {
-                'power': 23,
-                'dateStart': '2019-10-20',
-                'dateEnd': '2019-10-21'
-            }],
-            'dateSart': '2019-10-22',
-            'dateEnd': None,
-            'tertiaryPower':
-            {
-                'p1': None,
-                'p2': None,
-                'p3': None
-            },
-            'climaticZone': 'climaticZone-2',
-            'experimentalGroupUser': True,
-            'experimentalGroupUserTest': True,
-            'buildingData':
-            {
-                'buildingConstructionYear': 2014,
-                'dwellingArea': 196,
-                'propertyType': 'primary',
-                'buildingType': 'Apartment',
-                'dwellingPositionInBuilding': 'first_floor',
-                'dwellingOrientation': 'SE',
-                'buildingWindowsType': 'double_panel',
-                'buildingWindowsFrame': 'PVC',
-                'buildingCoolingSource': 'electricity',
-                'buildingHeatingSource': 'district_heating',
-                'buildingHeatingSourceDhw': 'gasoil',
-                'buildingSolarSystem': 'not_installed'
-            },
-            'customer':
-            {
-                'customerId': 'customerId-123',
-                'address':
-                {
-                    'buildingId': 'building-123',
-                    'city': 'city-123',
-                    'cityCode': 'cityCode-123',
-                    'countryCode': 'ES',
-                    'country': 'Spain',
-                    'postalCode': 'postalCode-123',
-                    'province': 'Barcelona',
-                    'provinceCode': 'provinceCode-123',
-                    'parcelNumber': 'parcelNumber-123',
-                }
-            },
-            'profile':
-            {
-                'totalPersonsNumber': 3,
-                'minorsPersonsNumber': 0,
-                'workingAgePersonsNumber': 2,
-                'retiredAgePersonsNumber': 1,
-                'malePersonsNumber': 2,
-                'femalePersonsNumber': 1,
-                'educationLevel':
-                {
-                    'edu_prim': 0,
-                    'edu_sec': 1,
-                    'edu_uni': 1,
-                    'edu_noStudies': 1
-                }
-            },
-            'activityCode': 'activityCode - activityCodeDescription',
-            'tariffId': 'tariffId-123',
-            'customisedGroupingCriteria':
-            {
-                'criteria_1': 'CLASS 1',
-                'criteria_2': 'XXXXXXX',
-                'criteria_3': 'YYYYYYY'
-            },
-            'customisedServiceParameters':
-            {
-                'OT701': 'p1;p2;px'
-            },
-            'meteringPointId': 'c1759810-90f3',
-            'devices':
-            [{
-                'dateStart': '2019-10-11T16:37:05Z',
-                'dateEnd': None,
-                'deviceId': 'c1810810-0381-012d-25a8-0017f2cd3574'
-            }],
-            'version': 1,
-            'report':
-            {
-                'initialMonth': '201902',
-                'language': 'ES'
-            }
-            }
+    return json4test['jsonContractById']
 
 
 def get_contracts(user, token_info, limit, from_, to_, tariff, juridic_type):
-
-    ContractsList = [
-        {
-            'contractId': '1',
-            'ownerId': 'ownerId-123',
-            'payerId': 'ownerId-123',
-            'power': 23,
-            'power_':
-            {
-                'power': 23,
-                'dateStart': '2019-10-20',
-                'dateEnd': '2019-10-21'
-            },
-            'powerHistory':
-            [{
-                'power': 23,
-                'dateStart': '2019-10-20',
-                'dateEnd': '2019-10-21'
-            },
-                {
-                'power': 23,
-                'dateStart': '2019-10-20',
-                'dateEnd': '2019-10-21'
-            }],
-            'dateSart': from_,
-            'dateEnd': to_,
-            'tertiaryPower':
-            {
-                'p1': None,
-                'p2': None,
-                'p3': None
-            },
-            'climaticZone': 'climaticZone-2',
-            'experimentalGroupUser': True,
-            'experimentalGroupUserTest': True,
-            'buildingData':
-            {
-                'buildingConstructionYear': 2014,
-                'dwellingArea': 196,
-                'propertyType': 'primary',
-                'buildingType': 'Apartment',
-                'dwellingPositionInBuilding': 'first_floor',
-                'dwellingOrientation': 'SE',
-                'buildingWindowsType': 'double_panel',
-                'buildingWindowsFrame': 'PVC',
-                'buildingCoolingSource': 'electricity',
-                'buildingHeatingSource': 'district_heating',
-                'buildingHeatingSourceDhw': 'gasoil',
-                'buildingSolarSystem': 'not_installed'
-            },
-            'customer':
-            {
-                'customerId': 'customerId-123',
-                'address':
-                {
-                    'buildingId': 'building-123',
-                    'city': 'city-123',
-                    'cityCode': 'cityCode-123',
-                    'countryCode': 'ES',
-                    'country': 'Spain',
-                    'postalCode': 'postalCode-123',
-                    'province': 'Barcelona',
-                    'provinceCode': 'provinceCode-123',
-                    'parcelNumber': 'parcelNumber-123',
-                }
-            },
-            'profile':
-            {
-                'totalPersonsNumber': 3,
-                'minorsPersonsNumber': 0,
-                'workingAgePersonsNumber': 2,
-                'retiredAgePersonsNumber': 1,
-                'malePersonsNumber': 2,
-                'femalePersonsNumber': 1,
-                'educationLevel':
-                {
-                    'edu_prim': 0,
-                    'edu_sec': 1,
-                    'edu_uni': 1,
-                    'edu_noStudies': 1
-                }
-            },
-            'activityCode': 'activityCode - activityCodeDescription',
-            'tariffId': 'tariffId-123',
-            'customisedGroupingCriteria':
-            {
-                'criteria_1': 'CLASS 1',
-                'criteria_2': 'XXXXXXX',
-                'criteria_3': 'YYYYYYY'
-            },
-            'customisedServiceParameters':
-            {
-                'OT701': 'p1;p2;px'
-            },
-            'meteringPointId': 'c1759810-90f3',
-            'devices':
-            [{
-                'dateStart': '2019-10-11T16:37:05Z',
-                'dateEnd': None,
-                'deviceId': 'c1810810-0381-012d-25a8-0017f2cd3574'
-            }],
-            'version': 1,
-            'report':
-            {
-                'initialMonth': '201902',
-                'language': 'ES'
-            }
-        },
-        {
-            'contractId': '2',
-            'ownerId': 'ownerId-123',
-            'payerId': 'ownerId-123',
-            'power': 23,
-            'power_':
-            {
-                'power': 23,
-                'dateStart': '2019-10-20',
-                'dateEnd': '2019-10-21'
-            },
-            'powerHistory':
-            [{
-                'power': 23,
-                'dateStart': '2019-10-20',
-                'dateEnd': '2019-10-21'
-            },
-                {
-                'power': 23,
-                'dateStart': '2019-10-20',
-                'dateEnd': '2019-10-21'
-            }],
-            'dateSart': from_,
-            'dateEnd': '2019-10-22',
-            'tertiaryPower':
-            {
-                'p1': None,
-                'p2': None,
-                'p3': None
-            },
-            'climaticZone': 'climaticZone-2',
-            'experimentalGroupUser': True,
-            'experimentalGroupUserTest': True,
-            'buildingData':
-            {
-                'buildingConstructionYear': 2014,
-                'dwellingArea': 196,
-                'propertyType': 'primary',
-                'buildingType': 'Apartment',
-                'dwellingPositionInBuilding': 'first_floor',
-                'dwellingOrientation': 'SE',
-                'buildingWindowsType': 'double_panel',
-                'buildingWindowsFrame': 'PVC',
-                'buildingCoolingSource': 'electricity',
-                'buildingHeatingSource': 'district_heating',
-                'buildingHeatingSourceDhw': 'gasoil',
-                'buildingSolarSystem': 'not_installed'
-            },
-            'customer':
-            {
-                'customerId': 'customerId-123',
-                'address':
-                {
-                    'buildingId': 'building-123',
-                    'city': 'city-123',
-                    'cityCode': 'cityCode-123',
-                    'countryCode': 'ES',
-                    'country': 'Spain',
-                    'postalCode': 'postalCode-123',
-                    'province': 'Barcelona',
-                    'provinceCode': 'provinceCode-123',
-                    'parcelNumber': 'parcelNumber-123',
-                }
-            },
-            'profile':
-            {
-                'totalPersonsNumber': 3,
-                'minorsPersonsNumber': 0,
-                'workingAgePersonsNumber': 2,
-                'retiredAgePersonsNumber': 1,
-                'malePersonsNumber': 2,
-                'femalePersonsNumber': 1,
-                'educationLevel':
-                {
-                    'edu_prim': 0,
-                    'edu_sec': 1,
-                    'edu_uni': 1,
-                    'edu_noStudies': 1
-                }
-            },
-            'activityCode': 'activityCode - activityCodeDescription',
-            'tariffId': 'tariffId-123',
-            'customisedGroupingCriteria':
-            {
-                'criteria_1': 'CLASS 1',
-                'criteria_2': 'XXXXXXX',
-                'criteria_3': 'YYYYYYY'
-            },
-            'customisedServiceParameters':
-            {
-                'OT701': 'p1;p2;px'
-            },
-            'meteringPointId': 'c1759810-90f3',
-            'devices':
-            [{
-                'dateStart': '2019-10-11T16:37:05Z',
-                'dateEnd': None,
-                'deviceId': 'c1810810-0381-012d-25a8-0017f2cd3574'
-            }],
-            'version': 1,
-            'report':
-            {
-                'initialMonth': '201902',
-                'language': 'ES'
-            }
-        }
-    ]
-    return ContractsList[:limit]
+    return json4test['jsonList'][:limit]
 
 
 if __name__ == '__main__':
