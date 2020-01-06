3b7a7a27de06066b435edfef4eec7300613c9c7e
Juan Pedro Sánchez <juanpe.sn@gmail.com>
2020-01-06 22:17:59 +0100

    added register methods

--- api/registration/login.py
+++ api/registration/login.py
@@ -22,7 +22,98 @@ async def authenticate(request):
     user = get_user(username)
 
     if user and pbkdf2_sha256.verify(password, user.password):
-        logger.info('user: {}'.format(user.username))
+        logger.info('user: %s', user.username)
         return user
 
     raise exceptions.AuthenticationFailed("Wrong user name or password")
+
+
+class InvitationUrlToken(HTTPMethodView):
+
+    async def get(self, request):
+        payload = {
+            'exp': datetime.utcnow() + timedelta(days=request.app.config.INVITATION_EXP_DAYS)
+        }
+        payload['is_superuser'] = request.args.get('is_superuser', False)
+
+        if request.args.get('id_partner'):
+            payload['id_partner'] = id_partner
+
+        with db_session:
+            invitation_token = InvitationToken(
+                inv_token=jwt.encode(
+                    payload, request.app.config.SECRET_KEY, algorithm='HS256'
+                ).decode(),
+                used=False
+            )
+
+        url = '{}://{}{}?invitation_token={}'.format(
+            request.scheme,
+            request.host,
+            request.app.url_for('auth_bp.Register'),
+            invitation_token.inv_token
+        )
+        return json(url)
+
+
+class Register(BaseEndpoint):
+
+    async def post(self, request):
+        invitation_token = request.args.get('invitation_token')
+        if not invitation_token:
+            return json(
+                {'exception': 'Invalid usage',
+                 'reasons': ['Invitation token not provided']
+                },
+                status=400
+            )
+        try:
+            with db_session:
+                token = select(
+                    token
+                    for token in InvitationToken
+                    if token.inv_token == invitation_token and token.used == False
+                ).first()
+                if not token:
+                    return json(
+                        {'exception': 'Invalid token',
+                         'reasons': ['Invitation token has been expired or is not valid']
+                        },
+                        status=400
+                    )
+                try:
+                    payload = jwt.decode(
+                        invitation_token, request.app.config.SECRET_KEY
+                    )
+                except jwt.ExpiredSignatureError as e:
+                    return json(
+                        {'exception': 'Invalid token',
+                         'reasons': ['Invitation has expired or is not valid']
+                        },
+                        status=400
+                    )
+                body = request.json
+                user = User(
+                    username=body['username'],
+                    password=pbkdf2_sha256.hash(body['password']),
+                    email=body['email'],
+                    id_partner=payload.get('id_partner'),
+                    is_superuser=payload.get('is_superuser', False)
+                )
+                commit()
+                token.used = True
+                result = user.to_dict()
+        except core.TransactionIntegrityError as e:
+            return json(
+                {'exception': 'User exception',
+                 'reasons': ['It\'s possible your user already exists']
+                },
+                status=400
+            )
+
+        return json(result)
+
+
+extra_views = (
+    ('/register', Register),
+)
--- api/registration/models.py
+++ api/registration/models.py
@@ -3,29 +3,40 @@ from pony.orm import Database, Optional, PrimaryKey, Required, db_session
 db = Database()
 
 
-db = Database()
+class InvitationToken(db.Entity):
+    _table_  = 'invitation_tokens'
 
-# set_sql_debug(True)
+    id = PrimaryKey(int, auto=True)
+
+    inv_token = Required(str)
+
+    used = Required(bool)
 
 
 class User(db.Entity):
     _table_ = 'user'
 
     id = PrimaryKey(int, auto=True)
-    username = Required(str)
+
+    username = Required(str, unique=True)
+
     password = Required(str)
+
+    email = Required(str)
+
     id_partner = Optional(int)
+
     is_superuser = Required(bool)
 
+    def to_dict(self):
+        attrs = ['id', 'username', 'email', 'id_partner', 'is_superuser']
+        return {
+            attr: getattr(self, attr, None)
+            for attr in attrs
+        }
 
-db.bind(
-    provider='sqlite',
-    filename=os.path.join(BASE_DIR, 'SomInfoenergia.sqlite'),
-    create_db=True
-)
-db.generate_mapping(create_tables=True)
 
-
+@db_session
 def get_user(username):
     user = User.get(username=username)
     return user
