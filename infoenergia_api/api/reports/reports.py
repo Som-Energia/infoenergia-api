dea65f70fb6eb71547adc595e121b012c70ecfaa
Marta <marta.fernandez@somenergia.coop>
2022-04-13 16:09:19 +0200

    move beedata api connection to app

--- infoenergia_api/api/reports/reports.py
+++ infoenergia_api/api/reports/reports.py
@@ -21,16 +21,8 @@ class ReportsView(PaginationLinksMixin, HTTPMethodView):
         logger.info("Uploading contracts")
         report_ids, month, report_type = await get_report_ids(request)
 
-        bapi = await BeedataApiClient.create(
-            url=request.app.config.BASE_URL,
-            username=request.app.config.USERNAME,
-            password=request.app.config.PASSWORD,
-            company_id=request.app.config.COMPANY_ID,
-            cert_file=request.app.config.CERT_FILE,
-            cert_key=request.app.config.KEY_FILE
-        )
         request.app.loop.create_task(
-            Beedata(bapi, request.app.mongo_client, request.app.redis).process_reports(report_ids, month, report_type)
+            Beedata(request.app.bapi, request.app.mongo_client, request.app.redis).process_reports(report_ids, month, report_type)
         )
         response = {
             'reports': len(report_ids),
